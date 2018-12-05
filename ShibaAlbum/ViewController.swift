5cb343552251f245b294d679335673c9d719ad1b
Jasmine Hung <jasminehung@defaultteki-macbook-pro.local>
2018-12-05 11:28:00 +0800

    Initial Commit

---
+++ ShibaAlbum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShibaAlbum
+//
+//  Created by Jasmine Hung on 2018/12/5.
+//  Copyright © 2018年 Jasmine Hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
