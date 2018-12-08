d852fa23f0b2a61df8e8c9a6ae1610ba14e541c8
Jasmine Hung <jasminehung@defaultteki-macbook-pro.local>
2018-12-08 11:44:53 +0800

    v1.1 Add snow flake emitter.

--- ShibaAlbum/ViewController.swift
+++ ShibaAlbum/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //建立並設定雪花
+        let snowFlakeEmitterCell = CAEmitterCell()
+        snowFlakeEmitterCell.contents = UIImage(named: "snowFlake")?.cgImage    //指定雪花圖案
+        snowFlakeEmitterCell.birthRate = 12                 //每秒產生12朵雪花
+        snowFlakeEmitterCell.lifetime = 60                  //每朵雪花的生命週期60秒
+        snowFlakeEmitterCell.velocity = 20                  //雪花初始移動速度20
+        snowFlakeEmitterCell.velocityRange = 200            //雪花移動的速度變化
+        snowFlakeEmitterCell.emissionRange = (45 * .pi/180) //向左右45度發射
+        snowFlakeEmitterCell.scale = 0.08                   //雪花的初始大小(圖片的0.08倍)
+        snowFlakeEmitterCell.scaleRange = 0.2               //雪花的大小變化
+        
+        //建立圖層
+        let emitterLayer = CAEmitterLayer()
+        emitterLayer.emitterPosition = CGPoint(x: view.bounds.width / 2, y: 0) //發射點位置
+        emitterLayer.emitterShape = CAEmitterLayerEmitterShape.line
+        emitterLayer.emitterSize = CGSize(width: view.bounds.width, height: 2)  //圖層大小
+        
+        //指定圖層內容是雪花
+        emitterLayer.emitterCells = [snowFlakeEmitterCell]
+        
+        //顯示圖層
+        view.layer.addSublayer(emitterLayer)
     }
-
 
 }
 
